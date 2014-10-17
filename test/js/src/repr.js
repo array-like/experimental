b4e48f9f5f4c578bd15c54598f16782e563dc7a1
aureooms <aurelien.ooms@gmail.com>
2014-10-17 14:32:25 +0200

    test for repr

---
+++ test/js/src/repr.js
@@ -0,0 +1,18 @@
+
+
+
+test( "repr", function () {
+
+	var a, b, n;
+
+	n = 10;
+
+	a = new Array( n );
+	b = new Uint8Array( n );
+
+	array.iota( a, 0, n, 0 );
+	array.iota( b, 0, n, 0 );
+
+	deepEqual( array.repr( b, 0, n ), JSON.stringify( a ), ":)" );
+
+});
