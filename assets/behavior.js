1738c1ba043511210b84b911c548f0d7e9846d81
aureooms <aurelien.ooms@gmail.com>
2015-07-03 20:54:35 +0200

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -289,6 +289,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "foldPrefix": "^",
           "name": "JavaScript"
         },
+        "sourcePath": "/home/aureooms/dev/js/js-array/js/src/fillfn.js",
+        "projectPath": "js/src/fillfn.js",
+        "targetPath": "fillfn",
+        "pageTitle": "fillfn",
+        "title": "fillfn"
+      },
+      "depth": 1,
+      "outline": []
+    }, {
+      "type": "file",
+      "data": {
+        "language": {
+          "nameMatchers": [".js"],
+          "pygmentsLexer": "javascript",
+          "multiLineComment": ["/*", "*", "*/"],
+          "singleLineComment": ["//"],
+          "ignorePrefix": "}",
+          "foldPrefix": "^",
+          "name": "JavaScript"
+        },
         "sourcePath": "/home/aureooms/dev/js/js-array/js/src/get.js",
         "projectPath": "js/src/get.js",
         "targetPath": "get",
